1cae2db16a9821035d59e0dfa334666e88918d28
kizitech <kizitoohani@gmail.com>
2024-03-20 13:34:19 +0100

    added the fourth step

--- src/Containers/SchoolManagementPagesContainer/SchoolCourseModules/CreateCourseFourthStep/CreateCourseFourthStep.tsx
+++ src/Containers/SchoolManagementPagesContainer/SchoolCourseModules/CreateCourseFourthStep/CreateCourseFourthStep.tsx
@@ -2,7 +2,7 @@ import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import Button from "../../../../Components/Button/Button";
 import classes from "../CreateCourseFirstStep/CreateCourseFirstStep.module.css";
 import SchoolCreatingLayout from "../../../../Components/SchoolCreatingLayout/SchoolCreatingLayout";
-import schoolImage from '../../../../Assets/Images/schoolImage.svg'
+import schoolImage from '../../../../Assets/Images/courseImage.svg'
 import { useState } from "react";
 import AcceptedModal from "../../../../Components/Modals/AcceptedModal/AcceptedModal";
 import CancelSchoolCreationModal from "./PreviewModals/CancelSchoolCreationModal";
@@ -15,9 +15,8 @@ type CreateCourseFourthStepProp = {
   name?: string;
   tagline?: string;
   description?: string;
-  school?: string;
   image?: string;
-  importanceItems?: string[];
+  objectives?: string[];
 }
 
 const CreateCourseFourthStep = ({
@@ -26,9 +25,8 @@ const CreateCourseFourthStep = ({
   name,
   tagline,
   description,
-  school,
   image,
-  importanceItems = []
+  objectives = [],
 }: CreateCourseFourthStepProp) => {
 
   // Router
@@ -89,39 +87,64 @@ const CreateCourseFourthStep = ({
         />
       )}
 
-      <SchoolCreatingLayout steps={[1, 2, 3]} showProgress={showIndicator}>
+      <SchoolCreatingLayout steps={[1, 2, 3, 4]} showProgress={showIndicator}>
 
         <section className={classes.container}>
-          <h2>{title || "Review school information"}</h2>
+          <h2>{title || "Review course cohort information"}</h2>
 
           <div className={classes.textSection}>
             <div>
-              <span>Name of school</span>
-              <p>{name || "iṣẹ́ School of Business"}</p>
+              <span>Name of Course</span>
+              <p>{name || "Talent Acquisition Course"}</p>
             </div>
             <div>
-              <span>School tagline</span>
-              <p>{tagline || "Help business grow"}</p>
+              <span>Course objectives</span>
+              <ul className={classes.listUl}>
+                {objectives.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
             </div>
             <div>
-              <span>School description</span>
-              <p>{description || "Ignite your business potential with our resources at iṣẹ́ School of Business. Gain the knowledge and skills to thrive in the dynamic world of commerce. Lead and achieve greatness with essential business skills."}</p>
+              <span>Course difficulty level</span>
+              <p>{tagline || "Beginner level course"}</p>
             </div>
             <div>
-              <span>School name</span>
-              <p>{school || "School of Business"}</p>
+              <span>Course description</span>
+              <p>{description || "Our Introduction to Talent Acquisition course is designed for beginners. This course covers fundamental principles, strategies, and best practices in recruiting top talent. Learn how to create effective job postings, conduct interviews, and make informed hiring decisions. Acquire the essential knowledge and tools to excel in your talent acquisition journey."}</p>
             </div>
             <div>
               <span>School image</span>
               <img src={image || schoolImage} alt="School cover" />
             </div>
-            <div>
-              <span>Importance of joining the school</span>
-              <ul>
-                {importanceItems.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
+            <div className={classes.cohort}>
+              <h4>Cohort infomation</h4>
+              <div className={classes.textSection}>
+                <div>
+                  <span>Cohort name</span>
+                  <p>{name || "Talent Acquisition May Cohort"}</p>
+                </div>
+                <div>
+                  <span>Application deadline</span>
+                  <p>{name || "12 Feb, 2024"}</p>
+                </div>
+                <div>
+                  <span>Start date</span>
+                  <p>{name || "12 May, 2024"}</p>
+                </div>
+                <div>
+                  <span>Cohort duration</span>
+                  <p>{name || "4 months"}</p>
+                </div>
+                <div>
+                  <span>Cohort tutor</span>
+                  <p>{name || "Olawuyi Justus"}</p>
+                </div>
+                <div>
+                  <span>Course price </span>
+                  <p>{name || "₦110,000"}</p>
+                </div>
+              </div>
             </div>
           </div>
 
@@ -137,7 +160,7 @@ const CreateCourseFourthStep = ({
             )}
             <Button
               type="secondary"
-              onClick={() => { setSearchParams({ step: "2" }); }}
+              onClick={() => { setSearchParams({ step: "3" }); }}
             >
               <span>Edit Information</span>
             </Button>
@@ -145,7 +168,7 @@ const CreateCourseFourthStep = ({
               <Button
                 onClick={() => { setDisplaySchoolCreatedSuccessfulModal(true) }}
               >
-                <span>Create School</span>
+                <span>Create Course</span>
               </Button>
             )}
           </div>
--- src/Pages/AddSchoolCoursePage.tsx
+++ src/Pages/AddSchoolCoursePage.tsx
@@ -12,6 +12,16 @@ const AddSchoolCoursePage = () => {
     // Router
     const location = useLocation();
     const userStep = new URLSearchParams(location.search).get("step");
+
+    const courseObjective = [
+        { list: "Develop a deep understanding of Talent Acquisition strategies and practices" },
+        { list: "Beginner-friendly program- no experience needed" },
+        { list: "Receive personalised one-on-one mentorship and guidance sessions" },
+        { list: "Gain practical skills to source, assess, and hire top talent effectively" },
+        { list: "Gain valuable skills for real-world projects" },
+        { list: "Complete course in 4 months at 10hrs/week" },
+    ];
+
     return (
         <Layout>
             {userStep === "1" ? (
@@ -21,7 +31,7 @@ const AddSchoolCoursePage = () => {
             ) : userStep === "3" ? (
                 <CreateCourseThirdStep />
             ) : userStep === "4" ? (
-                <CreateCourseFourthStep />
+                <CreateCourseFourthStep showIndicator={true} objectives={courseObjective.map(importance => importance.list)} />
             ) : (
                 <SchoolCreatedPage />
             )}
