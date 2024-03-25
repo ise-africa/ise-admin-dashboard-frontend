fe99cf43f50a89b0fd29a09b9025b2a2162e4eff
kizitech <kizitoohani@gmail.com>
2024-03-25 23:07:04 +0100

    added the courseInsights of the ContentAnalyticsBoardContainer

--- src/Containers/ContentControlManagementPagesContainer/ContentAnalyticsBoardContainer/ContentAnalyticsBoardContainer.tsx
+++ src/Containers/ContentControlManagementPagesContainer/ContentAnalyticsBoardContainer/ContentAnalyticsBoardContainer.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import classes from './ContentAnalyticsBoardContainer.module.css';
+import HelloUser from '../../../Components/HelloUser/HelloUser';
 
 const ContentAnalyticsBoardContainer = () => {
+
+    const courseInsights = [
+        {
+            title: 'Uploaded Modules',
+            count: 20,
+            note: 'Total modules uploaded by tutors'
+        },
+        {
+            title: 'Published modules',
+            count: 0,
+            note: 'Total modules accepted and published by administrator'
+        },
+        {
+            title: 'Declined modules',
+            count: 0,
+            note: 'Total modules declined by administrator '
+        },
+        {
+            title: 'Recent modules',
+            count: 0,
+            note: 'Total modules resubmitted by the tutor'
+        },
+    ];
+
     return (
-        <div>ContentAnalyticsBoardContainer</div>
-    )
-}
+        <div className={classes.container}>
+            <HelloUser
+                header="Frontend content analytics"
+                paragraph=""
+            />
+
+            <div className={classes.scrollContainer}>
+                <div className={classes.insight}>
+                    {courseInsights.map((data, i) => (
+                        <div key={i}>
+                            <p>{data.title}</p>
+                            <h2>{data.count}</h2>
+                            <span>{data.note}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* <CourseUnderReviewContainerTab /> */}
+        </div>
+    );
+};
 
 export default ContentAnalyticsBoardContainer
