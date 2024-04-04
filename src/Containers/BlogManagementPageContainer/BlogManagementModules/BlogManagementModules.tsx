f28e840c0571bcea829d43d823be465c1f06e2d3
kizitech <kizitoohani@gmail.com>
2024-04-04 15:44:23 +0100

    I added the empty tab for the blog management  post

--- src/Containers/BlogManagementPageContainer/BlogManagementModules/BlogManagementModules.tsx
+++ src/Containers/BlogManagementPageContainer/BlogManagementModules/BlogManagementModules.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import classes from "./BlogManagementModules.module.css"
+import { AppContext } from "../../../Context/AppContext";
+import React, { useContext, useEffect, useState } from 'react'
+import SectionsNav4 from '../../../Components/SectionsNav4/SectionsNav4';
+import SchoolCreatedContainer from '../../SchoolManagementPagesContainer/SchoolCreatedContainer/SchoolCreatedContainer';
+import BlogMangementModulesEmptyTab from './BlogMangementModulesEmptyTab';
 
 const BlogManagementModules = () => {
+    const { schools } = useContext(AppContext)
+
+    // States
+    const [navItems, setNavItems] = useState<any[]>([
+        {
+            title: "Published posts",
+            isActive: true,
+            statusFilter: "Published",
+            emptyState: {
+                header: "No blogposts",
+                paragraph: "Select “Create blogpost” to share insights, and connect with iṣẹ́’s audience. Once you publish a blogpost, it’ll appear here."
+            }
+        },
+        {
+            title: "Drafts",
+            isActive: false,
+            statusFilter: "Draft",
+            emptyState: {
+                header: "No blogpost drafts ",
+                paragraph: "Once you cancel or save a blogpost to continue later it’ll appear here"
+            }
+        },
+        {
+            title: "Archives",
+            isActive: false,
+            statusFilter: "Archive",
+            emptyState: {
+                header: "No archived blogposts",
+                paragraph: "Once you temporarily deactivate a blogpost it’ll will appear here until you delete it permanently."
+            }
+        },
+    ]);
+
+    const [filteredSchools, setFilteredSchools] = useState<any[]>(schools);
+
+    useEffect(() => {
+        const activeTab = navItems.find(item => item.isActive);
+        if (activeTab) {
+            if (activeTab.statusFilter) {
+                const filtered = schools.filter(school => school.status === activeTab.statusFilter);
+                setFilteredSchools(filtered);
+            } else {
+                setFilteredSchools(schools);
+            }
+        }
+    }, [navItems, schools]);
+
+    //   Utils
+    const activeComponent = navItems.find((data) => data.isActive);
     return (
-        <div>BlogManagementModules</div>
-    )
-}
+        <section className={classes.container}>
+            <div className={classes.sectionsNavSection}>
+                <SectionsNav4 navItems={navItems} setNavItems={setNavItems} />
+            </div>
+            <div className={classes.body}>
+                {filteredSchools.length > 0 ? (
+                    <SchoolCreatedContainer notShowEmptyTab={true} schools={filteredSchools} />
+                ) : (
+                    <BlogMangementModulesEmptyTab
+                        header={activeComponent.emptyState.header}
+                        paragraph={activeComponent.emptyState.paragraph}
+                    />
+                )}
+            </div>
+        </section>
+    );
+};
 
 export default BlogManagementModules
--- src/Containers/BlogManagementPageContainer/BlogManagementModules/BlogMangementModulesEmptyTab.tsx
+++ src/Containers/BlogManagementPageContainer/BlogManagementModules/BlogMangementModulesEmptyTab.tsx
@@ -17,9 +17,9 @@ const BlogMangementModulesEmptyTab = ({ header, paragraph }: BlogMangementModule
             <p>{paragraph}</p>
             <Button
                 type='null'
-                onClick={() => { navigate('/schools/add-school?step=1') }}>
+                onClick={() => { navigate('/blogs/add-post?step=1') }}>
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 1V17M17 9L1 9" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M9 1V17M17 9L1 9" stroke="#664EFE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
                 <span>Create blogpost</span>
             </Button>
